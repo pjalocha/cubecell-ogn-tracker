ce4cfde32ad0ccc1861bbec4460b7d5fffd529aa
Pawel Jalocha <Pawel.Jalocha@gmail.com>
2021-12-22 09:07:11 +0000

    FEC correction on reception, use own NMEA interpreter

--- src/cubecell-ogn-tracker.ino
+++ src/cubecell-ogn-tracker.ino
@@ -14,6 +14,8 @@
 #include "manchester.h"
 #include "ogn1.h"
 
+#include "rfm.h"
+
 static uint64_t getUniqueID(void) { return getID(); }        // get unique serial ID of the CPU/chip
 static uint32_t getUniqueAddress(void) { return getID()&0x00FFFFFF; }
 
@@ -77,7 +79,7 @@ int  CONS_UART_Read (uint8_t &Byte)
 
 // ===============================================================================================
 
-static NMEA_RxMsg NMEA;
+static NMEA_RxMsg ConsNMEA;
 
 static void PrintParameters(void)                               // print parameters stored in Flash
 { Parameters.Print(Line);                                       // single line, most essential parameters
@@ -94,11 +96,11 @@ static void PrintPOGNS(void)                                   // print paramete
   Parameters.WritePOGNS_Comp(Line);
   Format_String(CONS_UART_Write, Line); }
 
-static void NMEA_Process(void)
-{ if(!NMEA.isPOGNS()) return;
-  if(NMEA.hasCheck() && !NMEA.isChecked() ) return;
-  if(NMEA.Parms==0) { PrintPOGNS(); return; }
-  Parameters.ReadPOGNS(NMEA);
+static void ConsNMEA_Process(void)
+{ if(!ConsNMEA.isPOGNS()) return;
+  if(ConsNMEA.hasCheck() && !ConsNMEA.isChecked() ) return;
+  if(ConsNMEA.Parms==0) { PrintPOGNS(); return; }
+  Parameters.ReadPOGNS(ConsNMEA);
   PrintParameters();
   Parameters.WriteToFlash();
 }
@@ -112,46 +114,62 @@ static int CONS_Proc(void)
   { uint8_t Byte; int Err=CONS_UART_Read(Byte); if(Err<=0) break;
     Count++;
     if(Byte==0x03) CONS_CtrlC();                                // if Ctrl-C received: print parameters
-    NMEA.ProcessByte(Byte);
-    if(NMEA.isComplete())
-    { NMEA_Process();                                           // interpret the NMEA
-      NMEA.Clear(); }
+    ConsNMEA.ProcessByte(Byte);
+    if(ConsNMEA.isComplete())
+    { ConsNMEA_Process();                                           // interpret the ConsNMEA
+      ConsNMEA.Clear(); }
   }
   return Count; }
 
 // ===============================================================================================
 // Process GPS data
 
+static NMEA_RxMsg GpsNMEA;
+
+static int          GPS_Ptr = 0;
+static GPS_Position GPS_Pipe[2];
+
 static uint32_t GPS_Idle=0;
 
-static int GPS_Copy(void)
+static int GPS_Process(void)                           // process serial data stream from the GPS
 { int Count=0;
   for( ; ; )
-  { if(GPS.available()<=0) break;
-    uint8_t Byte=GPS.read();
-    GPS.encode(Byte); Serial.write(Byte);
-    Count++; }
-  return Count; }
+  { if(GPS.available()<=0) break;                      // if no more characters then give up
+    uint8_t Byte=GPS.read();                           // read character
+    // GPS.encode(Byte);                                 // process character through the GPS NMEA interpreter
+    GpsNMEA.ProcessByte(Byte);                         // NMEA interpreter
+    if(GpsNMEA.isComplete())                           // if NMEA is done
+    { GPS_Pipe[GPS_Ptr].ReadNMEA(GpsNMEA);             // interpret the NMEA by the GPS
+      GpsNMEA.Clear(); }
+    Serial.write(Byte);                                // copy character to the console (we could copy only the selected and correct sentences)
+    Count++; }                                         // count processed characters
+  return Count; }                                      // return number of processed characters
 
-static void GPS_Display(void)
+static void GPS_Next(void)
+{ int Next = GPS_Ptr^1;
+  if(GPS_Pipe[GPS_Ptr].isValid() && GPS_Pipe[Next].isValid()) GPS_Pipe[GPS_Ptr].calcDifferentials(GPS_Pipe[Next]);
+  GPS_Ptr=Next; }
+
+static void OLED_GPS(void)                             // display time, date and GPS data/status
 { Display.clear();
   // Display.setFont(ArialMT_Plain_10);
   Display.setFont(ArialMT_Plain_16);
+  const GPS_Position &GPS = GPS_Pipe[GPS_Ptr];
   char Line[16];
-  if(GPS.time.isValid())
-  { int Idx = sprintf(Line, "%02d:%02d:%02d", GPS.time.hour(), GPS.time.minute(), GPS.time.second() /* , GPS.time.centisecond() */ );
+  if(GPS.isTimeValid())
+  { int Idx = sprintf(Line, "%02d:%02d:%02d", GPS.Hour, GPS.Min, GPS.Sec);
     Line[Idx] = 0; }
-  else { strcpy(Line, "--:--:--.--"); }
+  else { strcpy(Line, "--:--:--"); }
   Display.setTextAlignment(TEXT_ALIGN_RIGHT);
   Display.drawString(128, 0, Line);
-  if(GPS.date.isValid())
-  { int Idx = sprintf(Line, "%02d.%02d.%02d", GPS.date.day(), GPS.date.month(), GPS.date.year()-2000);
+  if(GPS.isTimeValid() && GPS.isDateValid())
+  { int Idx = sprintf(Line, "%02d.%02d.%02d", GPS.Day, GPS.Month, GPS.Year);
     Line[Idx] = 0; }
-  else { strcpy(Line, "--.--.----"); }
+  else { strcpy(Line, "--.--.--"); }
   Display.setTextAlignment(TEXT_ALIGN_LEFT);
   Display.drawString(0, 0, Line);
-  // if(GPS.pos.isValid())
-  // { }
+  if(GPS.isValid())
+  { }
   Display.display(); }
 
 // ===============================================================================================
@@ -167,7 +185,7 @@ static void LED_Blue  (void) { Pixels.setPixelColor( 0,   0,   0, 255, 0); Pixel
 // ===============================================================================================
 // OGN packets
 
-static uint16_t InfoParmIdx = 0;            // the round-robin index to info records in info packets
+static uint16_t InfoParmIdx = 0;                                       // the round-robin index to info records in info packets
 
 static int ReadInfo(OGN1_Packet &Packet)
 { Packet.clrInfo();
@@ -185,13 +203,22 @@ static int ReadInfo(OGN1_Packet &Packet)
   Packet.setInfoCheck();
   return Packet.Info.DataChars; }                                      // zero => no info parameters were stored
 
-static int getInfoPacket(OGN1_Packet &Packet)
+static int getInfoPacket(OGN1_Packet &Packet)                          // produce info-type OGN packet
 { Packet.HeaderWord = 0;
-  Packet.Header.Address = Parameters.Address;
-  Packet.Header.AddrType = Parameters.AddrType;
+  Packet.Header.Address = Parameters.Address;                          // aircraft address
+  Packet.Header.AddrType = Parameters.AddrType;                        // aircraft address-type
   Packet.Header.NonPos = 1;
   Packet.calcAddrParity();
-  return ReadInfo(Packet); }
+  return ReadInfo(Packet); }                                           // encode the info from the parameters
+
+static int getPosPacket(OGN1_Packet &Packet)                           // produce position OGN packet
+{ Packet.HeaderWord = 0;
+  Packet.Header.Address = Parameters.Address;                          // aircraft address
+  Packet.Header.AddrType = Parameters.AddrType;                        // aircraft address-type
+  Packet.calcAddrParity();
+  Packet.Position.AcftType = Parameters.AcftType;                      // aircraft type
+  //
+  return 0; }
 
 // ===============================================================================================
 // Radio
@@ -206,27 +233,49 @@ static const uint8_t PAW_SYNC [10] = { 0xB4, 0x2B, 0x00, 0x00, 0x00, 0x00, 0x18,
 static RadioEvents_t Radio_Events;
 
 static void Radio_TxDone(void)
-{ Serial.printf("%d: Radio_TxDone()\n", millis());
+{ // Serial.printf("%d: Radio_TxDone()\n", millis());
   Radio.Rx(0); }
 
 static void Radio_TxTimeout(void)
-{ Serial.printf("%d: Radio_TxTimeout()\n", millis());
+{ // Serial.printf("%d: Radio_TxTimeout()\n", millis());
   Radio.Rx(0); }
 
+static LDPC_Decoder      Decoder;      // decoder and error corrector for the OGN Gallager/LDPC code
+static RFM_FSK_RxPktData RxPktData;
+static OGN_RxPacket<OGN1_Packet> RxPacket;
+
 static void Radio_RxDone( uint8_t *Packet, uint16_t Size, int16_t RSSI, int8_t SNR)
-{ Serial.printf("%d: Radio_RxDone( , %d, %d, %d)\n", millis(), Size, RSSI, SNR);
+{ if(Size!=2*26) return;
+  uint8_t PktIdx=0;
+  for(uint8_t Idx=0; Idx<26; Idx++)                                     // loop over packet bytes
+  { uint8_t ByteH = Packet[PktIdx++];
+    ByteH = ManchesterDecode[ByteH]; uint8_t ErrH=ByteH>>4; ByteH&=0x0F; // decode manchester, detect (some) errors
+    uint8_t ByteL = Packet[PktIdx++];
+    ByteL = ManchesterDecode[ByteL]; uint8_t ErrL=ByteL>>4; ByteL&=0x0F;
+    RxPktData.Data[Idx]=(ByteH<<4) | ByteL;
+    RxPktData.Err [Idx]=(ErrH <<4) | ErrL ;
+  }
+  RxPktData.Time = 0;
+  RxPktData.msTime = 0;
+  RxPktData.Channel = 0;
+  RxPktData.RSSI = -2*RSSI;
+  if(RxPktData.Decode(RxPacket, Decoder)==0 && RxPacket.RxErr<10)    // if FEC correctly decoded
+  {
+  }
+  Serial.printf("%d: Radio_RxDone( , %d, %d, %d) RxErr=%d %08X\n", millis(), Size, RSSI, SNR, RxPacket.RxErr, RxPacket.Packet.HeaderWord);
+  // RxPktData.Print(CONS_UART_Write, 1);
 }
 
 static void OGN_TxConfig(void)
 { Radio.SetTxConfig(MODEM_FSK, Parameters.TxPower, 50000, 0, 100000, 0, 1, 1, 0, 0, 0, 0, 20, 8, OGN1_SYNC); }
 
-static void PAW_TxConfig(void)
+static void PAW_TxConfig(void)      // incorrect because the PAW preamble needs to be long and the library does not support it
 { Radio.SetTxConfig(MODEM_FSK, Parameters.TxPower+6, 9600, 0, 38400, 0, 1, 1, 0, 0, 0, 0, 20, 8, PAW_SYNC); }
 
 static void OGN_RxConfig(void)
 { Radio.SetRxConfig(MODEM_FSK, 250000, 100000, 0, 250000, 1, 100, 1, 52, 0, 0, 0, 0, true, 8, OGN1_SYNC); }
 
-static int Radio_Transmit(const uint8_t *Data, uint8_t Len=26)
+static int OGN_Transmit(const uint8_t *Data, uint8_t Len=26)      // send packet, but first manchester encode it
 { uint8_t Packet[2*Len];
   uint8_t PktIdx=0;
   for(uint8_t Idx=0; Idx<Len; Idx++)
@@ -237,24 +286,32 @@ static int Radio_Transmit(const uint8_t *Data, uint8_t Len=26)
   Radio.Send(Packet, 2*Len);
   return 0; }
 
-static int Radio_Transmit(OGN_TxPacket<OGN1_Packet> &TxPacket)
-{ return Radio_Transmit(TxPacket.Byte(), TxPacket.Bytes); }
+static int OGN_Transmit(OGN_TxPacket<OGN1_Packet> &TxPacket)
+{ return OGN_Transmit(TxPacket.Byte(), TxPacket.Bytes); }
+
+// ===============================================================================================
+
+static bool Button_isPressed(void) { return digitalRead(USER_KEY)==0; }
+
+static void Button_Process(void)
+{ 
+}
 
 // ===============================================================================================
 
 void setup()
 { // delay(2000); // prevents USB driver crash on startup, do not omit this
 
-  VextON();                   // Turn on power to OLED
+  VextON();                               // Turn on power to OLED (and possibly other external devices)
   delay(100);
 
-  Serial.begin(115200);                   // console/debug UART
+  Serial.begin(115200);                   // Start console/debug UART
   // while (!Serial) { }                  // wait for USB serial port to connect
   Serial.println("OGN Tracker on HELTEC CubeCell");
 
-  pinMode(P3_3,INPUT);                    // push button
+  pinMode(USER_KEY, INPUT);               // push button
 
-  Pixels.begin();                         // RGB LED
+  Pixels.begin();                         // Start RGB LED
   Pixels.clear();
   Pixels.setBrightness(0x08);
   LED_Green();
@@ -263,7 +320,7 @@ void setup()
 
   Parameters.ReadFromFlash();             // read parameters from Flash
 
-  Display.init();                                   // OLED
+  Display.init();                         // Start the OLED
   Display.clear();
   Display.display();
   Display.setTextAlignment(TEXT_ALIGN_CENTER);      // 
@@ -271,9 +328,9 @@ void setup()
   Display.drawString(64, 32-16/2, "OGN-Tracker");
   Display.display();
 
-  GPS.begin(38400);                                  // GPS
+  GPS.begin(38400);                                  // Start the GPS
 
-  Radio_Events.TxDone    = Radio_TxDone;             // TRX
+  Radio_Events.TxDone    = Radio_TxDone;             // Start the Radio
   Radio_Events.TxTimeout = Radio_TxTimeout;
   Radio_Events.RxDone    = Radio_RxDone;
   Radio.Init(&Radio_Events);
@@ -285,32 +342,35 @@ void setup()
 
 static OGN_TxPacket<OGN1_Packet> TxPacket;
 
-static bool GPS_Done=0;
+static uint32_t GPS_Start = 0; // [ms]
+
+static bool GPS_Done = 0;      // State: 1 = GPS is sending data, 0 = GPS sent all data, waiting for the next PPS
 
 void loop()
 {
   CONS_Proc();
-
-  if(GPS_Copy()==0) { GPS_Idle++; delay(1); }
-               else { GPS_Idle=0; }
+  Button_Process();
+  if(GPS_Process()==0) { GPS_Idle++; delay(1); }
+                  else { GPS_Idle=0; }
   if(GPS_Done)
-  { if(GPS_Idle<3) { GPS_Done=0; LED_Blue(); } }
+  { if(GPS_Idle<3) { GPS_Done=0; GPS_Start=millis(); LED_Blue(); } } // GPS started sending data
   else
-  { if(GPS_Idle>5)
-    { if(GPS.date.isValid()) LED_Green();
+  { if(GPS_Idle>5)                                                // GPS stopped sending data
+    { if(GPS.date.isValid()) LED_Green();                         //
                        else  LED_Yellow();
       // here we start the slot just after the GPS sent its data
       // printf("%d: Batt\n", millis());
-      uint16_t BattVolt = getBatteryVoltage();                    // [mV]
+      uint16_t BattVolt = getBatteryVoltage();                    // read the battery voltage [mV]
       // printf("BattVolt=%d mV\n", BattVolt);
       // printf("%d: OLED\n", millis());
-      GPS_Display();                                              //
+      OLED_GPS();                                                 // display GPS data on the OLED
       if(getInfoPacket(TxPacket.Packet))
       { TxPacket.Packet.Whiten(); TxPacket.calcFEC();
         // printf("%d: Radio\n", millis());
-        Radio_Transmit(TxPacket); }
+        OGN_Transmit(TxPacket); }
       // printf("%d:\n", millis());
       LED_OFF();
+      GPS_Next();
       GPS_Done=1; }
   }
 
