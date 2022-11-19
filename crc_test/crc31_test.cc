4df4dde00a368c0c9b15b8301c578641c046937f
Pawel Jalocha <Pawel.Jalocha@gmail.com>
2022-11-19 17:52:58 +0000

    Correct up to 4 bit errors per signature - improves decodong but costs CPU

--- crc_test/crc31_test.cc
+++ crc_test/crc31_test.cc
@@ -90,33 +90,83 @@ static void SyndromeSort(void)
 { for(int Bit=0; Bit<SignBits; Bit++)
   { uint64_t Syndr = CRC31_Syndrome[Bit]; Syndr = (Syndr<<32) | Bit;
     CRC31_SyndromeSorted[Bit] = Syndr; }
-  std::sort(CRC31_SyndromeSorted, CRC31_SyndromeSorted+SignBits);
-  // for(int Bit=0; Bit<SignBits; Bit++)
-  // { printf("%4d: %08X:%4d\n", Bit, (uint32_t)(CRC31_SyndromeSorted[Bit]>>32), (uint16_t)CRC31_SyndromeSorted[Bit]); }
-}
+  std::sort(CRC31_SyndromeSorted, CRC31_SyndromeSorted+SignBits); }
 
 uint16_t CRC31_FindSyndrome(uint32_t Syndr)
-{ // printf("CRC31_FindSyndrome(%08X) ", (uint32_t)Syndr);
-  uint16_t Bot=0;
+{ uint16_t Bot=0;
   uint16_t Top=SignBits;
   uint32_t MidSyndr=0;
   for( ; ; )
   { uint16_t Mid=(Bot+Top)>>1;
     MidSyndr = CRC31_SyndromeSorted[Mid]>>32;
-    if(Mid==Bot) break;
-    // printf(" %d", Mid);
     if(Syndr==MidSyndr) return (uint16_t)CRC31_SyndromeSorted[Mid];
+    if(Mid==Bot) break;
     if(Syndr< MidSyndr) Top=Mid;
-                   else Bot=Mid;
-  }
-  // printf(" => %4d: %08X:%4d\n", Bot, (uint32_t)(CRC31_SyndromeSorted[Bot]>>32), (uint16_t)CRC31_SyndromeSorted[Bot]);
-  if(Syndr==MidSyndr) return (uint16_t)MidSyndr;
-  return 0xFFFF; }
+                   else Bot=Mid; }
+  return SignBits; }
+
+// ======================================================================================================
 
 static void FlipBit(uint8_t *Data, int Bit)  // flip a single bit of the packet
 { int Idx=Bit>>3; Bit=(Bit&7)^7;
   uint8_t Mask = 1<<Bit;
   Data[Idx]^=Mask; }
+
+static uint32_t BitWeiSorted[SignBits];
+
+static int SignErrCorr(uint8_t *Sign, uint32_t &Syndr, const float *RxSign, float RxAmpl)
+{ if(Syndr==0) return 0;
+  for(int Bit=0; Bit<SignBits; Bit++)
+  { int32_t BitWei = floor(RxSign[Bit]*1024/RxAmpl+0.5);
+    BitWei = abs(BitWei); if(BitWei>2048) BitWei=2048;
+    BitWeiSorted[Bit] = (BitWei<<16) | Bit; }
+  std::sort(BitWeiSorted, BitWeiSorted+SignBits);
+  for(int Idx=0; Idx<64; Idx++)
+  { uint16_t Bit1 = (uint16_t)BitWeiSorted[Idx];
+    uint16_t Wei1 = BitWeiSorted[Idx]>>16;
+    if(Wei1>=512) break;
+    uint16_t Bit2 = CRC31_FindSyndrome(Syndr^CRC31_Syndrome[Bit1]);
+    if(Bit2<SignBits)
+    { FlipBit(Sign, Bit1); Syndr^=CRC31_Syndrome[Bit1];
+      FlipBit(Sign, Bit2); Syndr^=CRC31_Syndrome[Bit2];
+      return 2; }
+  }
+  for(int Idx1=1; Idx1<64; Idx1++)
+  { uint16_t Bit1 = (uint16_t)BitWeiSorted[Idx1];
+    uint16_t Wei1  = BitWeiSorted[Idx1]>>16;
+    if(Wei1>=512) break;
+    for(int Idx2=0; Idx2<Idx1; Idx2++)
+    { uint16_t Bit2 = (uint16_t)BitWeiSorted[Idx2];
+      uint16_t Wei2  = BitWeiSorted[Idx2]>>16;
+      uint16_t Bit3 = CRC31_FindSyndrome(Syndr^CRC31_Syndrome[Bit1]^CRC31_Syndrome[Bit2]);
+      if(Bit3<SignBits)
+      { FlipBit(Sign, Bit1); Syndr^=CRC31_Syndrome[Bit1];
+        FlipBit(Sign, Bit2); Syndr^=CRC31_Syndrome[Bit2];
+        FlipBit(Sign, Bit3); Syndr^=CRC31_Syndrome[Bit3];
+        return 3; }
+    }
+  }
+  for(int Idx1=2; Idx1<64; Idx1++)
+  { uint16_t Bit1 = (uint16_t)BitWeiSorted[Idx1];
+    uint16_t Wei1  =          BitWeiSorted[Idx1]>>16;
+    if(Wei1>=512) break;
+    for(int Idx2=1; Idx2<Idx1; Idx2++)
+    { uint16_t Bit2 = (uint16_t)BitWeiSorted[Idx2];
+      // uint16_t Wei2  =          BitWeiSorted[Idx2]>>16;
+      for(int Idx3=0; Idx3<Idx2; Idx3++)
+      { uint16_t Bit3 = (uint16_t)BitWeiSorted[Idx3];
+        // uint16_t Wei3  =          BitWeiSorted[Idx3]>>16;
+        uint16_t Bit4 = CRC31_FindSyndrome(Syndr^CRC31_Syndrome[Bit1]^CRC31_Syndrome[Bit2]^CRC31_Syndrome[Bit3]);
+        if(Bit4<SignBits)
+        { FlipBit(Sign, Bit1); Syndr^=CRC31_Syndrome[Bit1];
+          FlipBit(Sign, Bit2); Syndr^=CRC31_Syndrome[Bit2];
+          FlipBit(Sign, Bit3); Syndr^=CRC31_Syndrome[Bit3];
+          FlipBit(Sign, Bit4); Syndr^=CRC31_Syndrome[Bit4];
+          return 3; }
+      }
+    }
+  }
+  return 0; }
 
 // ======================================================================================================
 
@@ -185,8 +235,13 @@ static int TRXsim(float Ampl=1.0, float Noise=0.5)
   { uint16_t BadBit = CRC31_FindSyndrome(Syndr);
     // printf("Syndr:%08X => [%04X]\n", Syndr, BadBit);
     if(BadBit<SignBits)
-    { FlipBit(DecodedSign, BadBit); Syndr^=CRC31_Syndrome[BadBit]; Count_FixedCRC++; }
+    { FlipBit(DecodedSign, BadBit);
+      Syndr^=CRC31_Syndrome[BadBit];
+      Count_FixedCRC++; }
   }
+  if(Syndr!=0)
+  { int ErrCorr=SignErrCorr(DecodedSign, Syndr, RxSign, Ampl);
+    if(ErrCorr) Count_FixedCRC++; }
   if(Syndr==0) Count_GoodCRC++;
           else Count_BadCRC++;
   float dB = 20.0*log10(Ampl/Noise);
@@ -200,23 +255,29 @@ static uint8_t Sign[SignBytes];               // signature to be transmitted, in
 int main(int argc, char *argv[])
 { time_t Now; time(&Now); srand(Now);
 
-  SetRandom(Sign, SignBytes);
-  uint32_t CRC = SetSignCRC(Sign);
+  SetRandom(Sign, SignBytes);                   // a random signature
+  uint32_t CRC = SetSignCRC(Sign);              // add the CRC on the last 31 bits
   // printf("Sign = "); PrintBytes(Sign, SignBytes); printf(" CRC=%08X\n", CRC);
 
+  // check syndrome for correct frames and with single bit errors
   uint32_t Check = CheckSignCRC(Sign);
   // printf("[----] = %08X\n", Check);
   for(int Bit=0; Bit<SignBits; Bit++)
   { FlipBit(Sign, Bit);
     uint32_t Check = CheckSignCRC(Sign);
     // printf("[%4d] = %08X\n", Bit, Check);
-    CRC31_Syndrome[Bit]=Check;
+    CRC31_Syndrome[Bit]=Check;                  // store single bit error syndrome in a tabel
     FlipBit(Sign, Bit); }
-  SyndromeSort();
+  SyndromeSort();                               // produce sorted table for a quick search
 
-  float Ampl=1.0;
-  float Step=pow(10.0, 0.05);
-  for(float Noise=0.1; Noise<0.6; Noise*=Step)
+  // test if syndrome search es correctly
+  for(int Bit=0; Bit<SignBits; Bit++)
+  { int ErrBit=CRC31_FindSyndrome(CRC31_Syndrome[Bit]);
+    if(ErrBit!=Bit) printf("CRC31_FindSyndrome() failed %d<=>%d\n", Bit, ErrBit); }
+
+  float Ampl=1.0;                               // simulate bit errors and packet errors
+  float Step=pow(10.0, 0.05);                   // step by 1dB
+  for(float Noise=0.1; Noise<0.6; Noise*=Step)  // start with 10:1 amplitude thus 20dB SNR
   { Count_Clear();
     for(int Test=0; Test<10000; Test++)
     { TRXsim(Ampl, Noise); }
