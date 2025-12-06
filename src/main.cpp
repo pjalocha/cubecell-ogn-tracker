919e100513b62854d28b2a0f08b13407d9c900c8
Pawel Jalocha <Pawel.Jalocha@gmail.com>
2025-12-06 19:03:02 +0000

    Convert .ino to .cpp to prepare to clean the code

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@
 #include "ogn1.h"
 #include "crc1021.h"
 #include "freqplan.h"
-#include "rfm.h"
+// #include "rfm.h"
 
 #ifdef WITH_ADSL
 #include "adsl.h"
@@ -104,6 +104,80 @@ static uint32_t getUniqueAddress(void) { return getID()&0x00FFFFFF; }
 #include "parameters.h"
 
 static FlashParameters Parameters;       // parameters stored in Flash: address, aircraft type, etc.
+
+// ===============================================================================================
+
+static void Button_ForceActive(void);
+
+static void OGN_TxConfig(void);
+static void OGN_RxConfig(void);
+
+class RFM_FSK_RxPktData             // OGN packet received by the RF chip
+{ public:
+   static const uint8_t Bytes=26;   // [bytes] number of bytes in the packet
+   uint32_t Time;                   // [sec] Time slot
+   uint16_t msTime;                 // [ms] reception time since the PPS[Time]
+   uint8_t Channel;                 // [   ] channel where the packet has been recieved
+   uint8_t RSSI;                    // [-0.5dBm] receiver signal strength
+   uint8_t Data[Bytes];             // Manchester decoded data bits/bytes
+   uint8_t Err [Bytes];             // Manchester decoding errors
+
+  public:
+
+   void Print(void (*CONS_UART_Write)(char), uint8_t WithData=0) const
+   { // uint8_t ManchErr = Count1s(RxPktErr, 26);
+     Format_String(CONS_UART_Write, "RxPktData: ");
+     Format_HHMMSS(CONS_UART_Write, Time);
+     CONS_UART_Write('+');
+     Format_UnsDec(CONS_UART_Write, msTime, 4, 3);
+     CONS_UART_Write(' '); Format_Hex(CONS_UART_Write, Channel);
+     CONS_UART_Write('/');
+     Format_SignDec(CONS_UART_Write, (int16_t)(-5*(int16_t)RSSI), 3, 1);
+     Format_String(CONS_UART_Write, "dBm\n");
+     if(WithData==0) return;
+     for(uint8_t Idx=0; Idx<Bytes; Idx++)
+     { CONS_UART_Write(' '); Format_Hex(CONS_UART_Write, Data[Idx]); }
+     CONS_UART_Write('\r'); CONS_UART_Write('\n');
+     for(uint8_t Idx=0; Idx<Bytes; Idx++)
+     { CONS_UART_Write(' '); Format_Hex(CONS_UART_Write, Err[Idx]); }
+     CONS_UART_Write('\r'); CONS_UART_Write('\n');
+   }
+
+   bool NoErr(void) const
+   { for(uint8_t Idx=0; Idx<Bytes; Idx++)
+       if(Err[Idx]) return 0;
+     return 1; }
+
+   uint8_t ErrCount(void) const                         // count detected manchester errors
+   { uint8_t Count=0;
+     for(uint8_t Idx=0; Idx<Bytes; Idx++)
+       Count+=Count1s(Err[Idx]);
+     return Count; }
+
+   uint8_t ErrCount(const uint8_t *Corr) const          // count errors compared to data corrected by FEC
+   { uint8_t Count=0;
+     for(uint8_t Idx=0; Idx<Bytes; Idx++)
+       Count+=Count1s((uint8_t)((Data[Idx]^Corr[Idx])&(~Err[Idx])));
+     return Count; }
+
+ template <class OGNx_Packet>
+  uint8_t Decode(OGN_RxPacket<OGNx_Packet> &Packet, LDPC_Decoder &Decoder, uint8_t Iter=32) const
+  { uint8_t Check=0;
+    uint8_t RxErr = ErrCount();                                // conunt Manchester decoding errors
+    Decoder.Input(Data, Err);                                  // put data into the FEC decoder
+    for( ; Iter; Iter--)                                       // more loops is more chance to recover the packet
+    { Check=Decoder.ProcessChecks();                           // do an iteration
+      if(Check==0) break; }                                    // if FEC all fine: break
+    Decoder.Output(Packet.Packet.Byte());                      // get corrected bytes into the OGN packet
+    RxErr += ErrCount(Packet.Packet.Byte());
+    if(RxErr>15) RxErr=15;
+    Packet.RxErr  = RxErr;
+    Packet.RxChan = Channel;
+    Packet.RxRSSI = RSSI;
+    Packet.Correct= Check==0;
+    return Check; }
+
+} ;
 
 // ===============================================================================================
 
@@ -1056,8 +1130,8 @@ static void ADSL_TxConfig(void)  // RF chip config for ADS-L transmissions: iden
 #endif
 
 #ifdef WITH_PAW
-static void PAW_TxConfig(void)  // RF chip config for PilotAWare transmissions: +/-9.6kHz, 38400bps, long preamble
-{ Radio.SetTxConfig(MODEM_FSK, Parameters.TxPower+6, 9600, 0, 38400, 0, 10, 1, 0, 0, 0, 0, 20);
+static void PAW_TxConfig(void)  // RF chip config for PilotAWare transmissions: +/-12.5kHz, 38400bps, long preamble
+{ Radio.SetTxConfig(MODEM_FSK, Parameters.TxPower+8, 12500, 0, 38400, 0, 10, 1, 0, 0, 0, 0, 20);
   Radio_UpdateConfig(PAW_SYNC, 8, MOD_SHAPING_G_BT_05); }
 #endif
 
